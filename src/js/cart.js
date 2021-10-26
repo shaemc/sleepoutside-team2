b8f4f490803c4ff2357d2dcb98e8b73f2b5feeb5
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-10-26 00:52:54 -0600

    fixed styles for little x on items in the cart page. Added functionality to remove an item from the cart when user clicks on the cross

--- src/js/cart.js
+++ src/js/cart.js
@@ -8,7 +8,7 @@ function getCartContents() {
   let markup = "";
   const cartItems = getLocalStorage("so-cart");
 
-  if (cartItems) {
+  if (cartItems.length > 0) {
     const htmlItems = cartItems.map((item) => renderCartItem(item));
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
 
@@ -18,15 +18,17 @@ function getCartContents() {
       .map((item) => (total += item.FinalPrice))
       .pop();
 
-    const showTotal = `<h3>Total: $${finalTotal}<h3/>`;
+    const showTotal = `<h3>Total: $${finalTotal.toFixed(2)}<h3/>`;
+    document.querySelector(".products").innerHTML += showTotal;
+  } else {
+    document.querySelector(".product-list").innerHTML = "";
+    const showTotal = "<h3>Total: $0<h3/>";
     document.querySelector(".products").innerHTML += showTotal;
   }
-
-  // document.querySelector('.product-list').innerHTML = renderCartItem(cartItems);
 }
 
 function renderCartItem(item) {
-  const newItem = `<li class='cart-card divider'>
+  const newItem = `<li class='cart-card divider' data-id='${item.Id}'>
   <a href='#' class='cart-card__image'>
     <img
       src='${item.Images.PrimarySmall}'
@@ -39,12 +41,44 @@ function renderCartItem(item) {
   <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
   <p class='cart-card__quantity'>qty: 1</p>
   <p class='cart-card__price'>$${item.FinalPrice}</p>
-  <p id='remove'>x</p>
+  <p class='remove'>x</p>
 </li>`;
 
   return newItem;
 }
 
 getCartContents();
-
 loadHeaderFooter();
+
+function removeItemFromCart(e) {
+  const itemId = e.path[1].dataset.id;
+
+  const localStorageItems = getLocalStorage("so-cart");
+
+  let newCartItems = [];
+
+  localStorageItems.map((item) => {
+    if (item.Id != itemId) {
+      newCartItems.push(item);
+    }
+  });
+
+  localStorage.setItem("so-cart", JSON.stringify(newCartItems));
+
+  const headings = [...document.querySelector(".products").children];
+  headings[2].remove();
+  headings[3].remove();
+
+  getCartContents();
+  addRemoveListener();
+}
+
+function addRemoveListener() {
+  const removeItemsList = [...document.querySelectorAll(".remove")];
+
+  removeItemsList.map((item) =>
+    item.addEventListener("click", removeItemFromCart)
+  );
+}
+
+addRemoveListener();
